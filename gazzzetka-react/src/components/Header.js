390677e91e71e5c38e1e7ecb30f0626fe7985204
Yevhen Lehkiy <yevhen.lehkyi@completecase.com>
2025-01-20 23:04:44 +0200

    Implement initial files

---
+++ gazzzetka-react/src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <header>Welcome to My React Project</header>;
+};
+
+export default Header;
---
+++ gazzzetka-react/src/pages/Home.js
@@ -0,0 +1,6 @@
+const Home = () => {
+    return <div>Welcome to the Home Page</div>;
+  };
+  
+  export default Home;
+  
